93328f7392fb883c499b42dccf76ef08c23eef3e
Julian de Jesus Lopez Lopez <julian.lz@ibm.com>
2020-04-22 23:01:07 -0500

    added asset chart, chart in liveTime component service created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LiveTimeComponent } from './live-time/live-time.component';
+
 
 const routes: Routes = [
-   { path: 'liveTime', component:  NavbarComponent}
+   { path: 'liveTime', component:  LiveTimeComponent}
 ];
 
 @NgModule({
--- src/app/live-time/live-time.component.ts
+++ src/app/live-time/live-time.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+import * as CanvasJS from '../../assets/canvasjs.min.js';
 
 @Component({
   selector: 'app-live-time',
@@ -10,6 +12,57 @@ export class LiveTimeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let dataPoints = [];
+    let dpsLength = 0;
+    let chart = new CanvasJS.Chart("chartContainer", {
+      exportEnabled: true,
+      title: {
+        text: "It woooooooooorks"
+      },
+      data: [{
+        type: "spline",
+        dataPoints: dataPoints,
+      }]
+    });
+
+    let chart2 = new CanvasJS.Chart("chartContainer2", {
+      exportEnabled: true,
+      title: {
+        text: "It woooooooooorks"
+      },
+      data: [{
+        type: "spline",
+        dataPoints: dataPoints,
+      }]
+    });
+
+    $.getJSON("https://canvasjs.com/services/data/datapoints.php?xstart=1&ystart=25&length=20&type=json&callback=?", function (data) {
+      $.each(data, function (key, value) {
+        dataPoints.push({ x: value[0], y: parseInt(value[1]) });
+      });
+      dpsLength = dataPoints.length;
+      chart.render();
+      chart2.render();
+      updateChart();
+    });
+    function updateChart() {
+      $.getJSON("https://canvasjs.com/services/data/datapoints.php?xstart=" + (dpsLength + 1) + "&ystart=" + (dataPoints[dataPoints.length - 1].y) + "&length=1&type=json&callback=?", function (data) {
+        $.each(data, function (key, value) {
+          dataPoints.push({
+            x: parseInt(value[0]),
+            y: parseInt(value[1])
+          });
+          dpsLength++;
+        });
+
+        if (dataPoints.length > 20) {
+          dataPoints.shift();
+        }
+        chart.render();
+        chart2.render();
+        setTimeout(function () { updateChart() }, 1000);
+      });
+    }
   }
 
 }
